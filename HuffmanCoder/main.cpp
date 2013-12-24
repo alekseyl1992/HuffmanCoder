1e7f5b1022eb9e12f4eae166d163b01d84100926
alekseyl1992 <alekseyl@list.ru>
2013-12-24 19:57:56 +0400

    Console interface added

--- HuffmanCoder/main.cpp
+++ HuffmanCoder/main.cpp
@@ -7,22 +7,55 @@ int main(int argc, char **argv)
 {
     HuffmanCoder coder;
 
-    std::ifstream in("CUU.doc", std::ios::binary);
-    std::ofstream out("CUU.doc.encoded", std::ios::binary);
+    std::string command;
+    std::string name;
+    std::string newName;
 
-    coder.encode(in, out);
+    if (argc == 4) //program pack|unpack file.name result.name
+    {
+        command = argv[1];
+        name = argv[2];
+        newName = argv[3];
+    }
+    else if(argc == 2) //program file.name
+    {
+        name = argv[1];
+        newName = name;
 
-    in.close();
-    out.close();
+        std::string extension = name.substr(name.rfind("."));
+        if (extension == ".hc")
+        {
+            command = "unpack";
+            newName = name.substr(0, name.length() - 3); //slice ".hc"
+        }
+        else
+        {
+            command = "pack";
+            newName += ".hc";
+        }
+    }
+    else
+    {
+        std::cout << "Usage:\n   HuffmanCoder.exe <pathToFile>\n or\n   HuffmanCoder.exe pack|unpack <pathToFile> <pathToResult>" << std::endl;
 
-    std::ifstream coded("CUU.doc.encoded", std::ios::binary);
-    std::ofstream decoded("CUU.decoded.doc", std::ios::binary);
-
-    coder.decode(coded, decoded);
-
-    coded.close();
-    decoded.close();
+        std::cout << std::endl << "Press any Enter to exit..." << std::endl;
+        std::cin.peek();
+        return 1;
+    }
 
 
-    system("pause");
+    if (command == "pack")
+    {
+        std::ifstream in(name, std::ios::binary);
+        std::ofstream out(newName, std::ios::binary);
+
+        coder.encode(in, out);
+    }
+    else if (command == "unpack")
+    {
+        std::ifstream in(name, std::ios::binary);
+        std::ofstream out(newName, std::ios::binary);
+
+        coder.decode(in, out);
+    }
 }
